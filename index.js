7c7d7758bbfb0b9d7f26aee44fc12f8379522738
sergio.ramirez@outcodesoftware.com <sergio.ramirez@Sergios-MacBook-Pro.local>
2024-04-28 13:08:27 -0500

    changes

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { Client } = require("@opensearch-project/opensearch");
+
+const client = new Client({
+  node: "http://localhost:9200",
+  auth: {
+    username: "admin",
+    password: "Kirochka1982?",
+  },
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  try {
+    const { start_date: startDate, end_date: endDate } = req.query;
+    const response = await client.search({
+      index: "test2",
+      body: {
+        size: "2000",
+        query: {
+          match_all: {},
+        },
+        sort: [
+          {
+            TS: { 
+              order: 'asc'
+            }
+          }
+        ]
+      },
+    });
+    res.json(response.body.hits);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 app.listen(8080);
